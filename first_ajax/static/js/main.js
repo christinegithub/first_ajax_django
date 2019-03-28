6d7e463d096441e5e06eb812e2cf5eeea2e3896c
christinegithub <christine13@gmail.com>
2019-03-28 13:28:54 -0400

    step 6 complete

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -8,13 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const pingpong = document.getElementById('pingpong');
   pingpong.addEventListener('click', function() {
-    axios.get('http://intro-ajax-api.herokuapp.com/ping')
+    axios.get('http://intro-ajax-api.herokuapp.com/pong')
     .then(function (response) {
-      // console.log(response.data);
+      console.log(response.data);
       const data = document.createElement('section');
       data.innerHTML = response.data;
       document.body.appendChild(data);
-    })
-  })
-
+    }).catch(function(response) {
+        const error = document.createElement('section');
+        error.innerText = "Sorry, I'll try harder next time";
+        document.body.appendChild(error);
+      }).then(function(response) {
+        console.log("Hey the request finised!");
+    });
+  });
 });
