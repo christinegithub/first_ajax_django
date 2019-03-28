60460be57f3fcc786f1e81fc29417b188e946379
christinegithub <christine13@gmail.com>
2019-03-28 13:02:54 -0400

    added .then

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+
+  const button = document.getElementById('button');
+  button.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/');
+
+  });
+
+  const pingpong = document.getElementById('pingpong');
+  pingpong.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/ping')
+    .then(function (response) {
+      // console.log(response.data);
+      const data = document.createElement('section');
+      data.innerHTML = response.data;
+      document.body.appendChild(data);
+    })
+  })
+
 });
